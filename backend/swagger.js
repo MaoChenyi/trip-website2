240245150666009a157597d2c8bab9d16106f922
MaoChenyi <m3124655493@gmail.com>
2025-07-15 21:51:44 +0900

    Update swagger.js

--- backend/swagger.js
+++ backend/swagger.js
@@ -1,26 +1,25 @@
 const swaggerJSDoc = require('swagger-jsdoc');
+const swaggerUi = require('swagger-ui-express');
 
 const options = {
   definition: {
-    components: {
-      securitySchemes: {
-        bearerAuth: {
-          type: 'http',
-          scheme: 'bearer',
-          bearerFormat: 'JWT'
-        }
-      }
-    },
-    security: [{ bearerAuth: [] }]
     openapi: '3.0.0',
     info: {
       title: 'Hotel Booking API',
       version: '1.0.0',
-      description: '酒店预订系统后端API文档'
-    }
+      description: '仿Trip.com酒店机票预订系统后端API文档'
+    },
+    servers: [
+      { url: 'http://localhost:3000' }
+    ]
   },
-  apis: ['./routes/*.js']
+  apis: ['./routes/*.js'],
 };
 
 const swaggerSpec = swaggerJSDoc(options);
-module.exports = swaggerSpec; 
+
+function setupSwagger(app) {
+  app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+}
+
+module.exports = setupSwagger;
